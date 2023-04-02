5e91597df5de02244487261644217eec8b45dec5
Quentinho199 <97668671+Quentinho199@users.noreply.github.com>
2023-04-02 19:18:12 +0200

    fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,5 @@
 export const environment = {
-<<<<<<< HEAD
   production: true,
   apiBoxesUrl: 'https://quizbe.org/api/boxes/',
   apiImageUrl: 'https://quizbe.org/api/image/',
-=======
-  production: false,
-  apiBoxesUrl: 'http://localhost:8080/api/boxes/',
-  apiImageUrl: 'http://localhost:8080/api/image/',
->>>>>>> origin/master
 };
